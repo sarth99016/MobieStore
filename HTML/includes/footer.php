5007d0e31f83f53023d176885ba04347f3b48880
sarth99016 <47793435+sarth99016@users.noreply.github.com>
2020-10-06 10:43:18 +0530

    Add files via upload

---
+++ HTML/includes/footer.php
@@ -0,0 +1,34 @@
+<!--Footer-->
+<div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <ul style="list-style-type:none;">
+                        <li>
+                            <h4>Information</h4>
+                        </li>
+                        <li><a style="color:white" href="./aboutus.html">About Us</a></li>
+                        <li><a style="color:white" href="./contactus.html">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <ul style="list-style-type:none;">
+                        <li>
+                            <h4>My account</h4>
+                        </li>
+                        <li><a style="color:white" data-toggle="modal" data-target="#loginmodal">Login</a></li>
+                        <li><a style="color:white" href="./signup.html">Sign Up</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <ul style="list-style-type:none;">
+                        <li>
+                            <h4>Contact Us</h4>
+                        </li>
+                        <li>Contact : +91-9000000000
+                </div>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </div>
